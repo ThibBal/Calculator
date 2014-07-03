5e2912441f09f7286ef994bcdf920eada1b3d55c
Kader SARRE <ksarre@prismamedia.com>
2014-07-03 09:52:31 +0200

    load des classes nécessaires pour les tests

--- Operators/test/bootstrap.php
+++ Operators/test/bootstrap.php
@@ -1,6 +1,14 @@
 <?php
 
-include_once('AutoLoader.php');
-// Register the directory to your include files
-var_dump(is_dir('../../Operators'));
-AutoLoader::registerDirectory('../src');
+
+require_once(__DIR__.'/vendor/autoload.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/AbstractOperation.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/OperationInterface.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/Addition.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/Division.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/Exception/DomainException.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/Exception/InvalidArgumentException.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/Exception/RuntimeException.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/Multiplication.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/NombreEntierPositif.php');
+require_once('/Users/Kader/Calculator/Operators/src/Maths/Soustraction.php');
